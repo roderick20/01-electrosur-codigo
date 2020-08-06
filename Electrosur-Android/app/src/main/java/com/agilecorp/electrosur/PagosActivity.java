c851106c63f54c18e1fc0c334a7f5b6896e17a8b
roderick20 <roderick20@hotmail.com>
2020-08-05 23:38:52 -0500

    Android - Manuales

---
+++ Electrosur-Android/app/src/main/java/com/agilecorp/electrosur/PagosActivity.java
@@ -0,0 +1,14 @@
+package com.agilecorp.electrosur;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PagosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagos);
+    }
+}
